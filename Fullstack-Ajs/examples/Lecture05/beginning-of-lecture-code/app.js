b4e0c5b6b7fff41ecf1132b488202971561343c9
user@KEVIN-OCHIENG <User.Kevinsky103@gmail.com>
2021-06-04 00:00:12 +0300

    modul1 sltn

--- Fullstack-Ajs/examples/Lecture05/beginning-of-lecture-code/app.js
+++ Fullstack-Ajs/examples/Lecture05/beginning-of-lecture-code/app.js
@@ -3,8 +3,8 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function () {
-
+.controller('MyFirstController', function ($scope) {
+$scope.name ="Kevin";
 });
 
 })();
---
+++ module1-solution/README.txt
@@ -0,0 +1,3 @@
+Solution to Module 1  assignment on the course "Single Page Web Application with AngularJS" at Coursera.
+
+
